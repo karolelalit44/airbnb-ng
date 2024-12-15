cfb06a8bd1ea3d6c87568241f1ef6c376e5abe2f
Lalit karole <lalit.karole@digimindtechnology.com>
2024-12-15 20:54:35 +0530

    header - user card add and style chores

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  imports:[CommonModule],
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isUserCardOn = false;
+
+  toggleCard() {
+    this.isUserCardOn = !this.isUserCardOn;
+  }
 
 }
