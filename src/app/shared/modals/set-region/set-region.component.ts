fff85c703b19c05b8b64b08d5e10999cae9ab29b
Lalit karole <lalit.karole@digimindtechnology.com>
2024-12-16 23:30:57 +0530

    overlap effect on set-region component

--- src/app/shared/modals/set-region/set-region.component.ts
+++ src/app/shared/modals/set-region/set-region.component.ts
@@ -8,14 +8,21 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './set-region.component.html',
   styleUrl: './set-region.component.css'
 })
+
 export class SetRegionComponent {
   @Output() close = new EventEmitter<void>();
-
+  isModalOpen= false
   activeSection: 'languageRegion' | 'currency' = 'languageRegion';
 
   closeModal() {
+    this.isModalOpen = false;
     this.close.emit();
   }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
 
   setActiveSection(section: 'languageRegion' | 'currency') {
     this.activeSection = section;
